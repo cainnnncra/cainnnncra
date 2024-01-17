cf943eb5f1ff8afaabb83134798401f2f1f52a73
cainnnncra <154397778+cainnnncra@users.noreply.github.com>
2024-01-17 10:33:18 +0530

    Add files via upload
    lol

---
+++ Sub.java
@@ -0,0 +1,14 @@
+package what;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10;
+		int num2=5;
+		int sub=  num1- num2;
+		System.out.println("The difference is"+sub);
+
+	}
+
+}
